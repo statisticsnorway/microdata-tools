79a1e015eebe76e613921c82899606ea15e11df7
Jon Haraldsvik <haraldsvik91@gmail.com>
2023-06-21 15:32:42 +0200

    Validation checks to package_dataset

--- microdata_tools/package_dataset.py
+++ microdata_tools/package_dataset.py
@@ -4,6 +4,8 @@ import shutil
 from pathlib import Path
 
 from microdata_tools._encrypt import _tar_encrypted_dataset, _encrypt_dataset
+from microdata_tools.exceptions import ValidationException
+from microdata_tools._utils import check_exists
 
 logger = logging.getLogger()
 
@@ -34,19 +36,36 @@ def package_dataset(
     dataset_output_dir = output_dir / dataset_name
     csv_files = [file for file in dataset_dir.iterdir() if file.suffix == ".csv"]
 
-    if len(csv_files) == 1:
-        _encrypt_dataset(
-            rsa_keys_dir=rsa_keys_dir,
-            dataset_dir=dataset_dir,
-            output_dir=output_dir,
-            chunk_size_bytes=chunk_size_bytes,
-        )
-    else:
-        if not dataset_output_dir.exists():
-            os.makedirs(dataset_output_dir)
+    try:
+        # check if json exists
+        check_exists(dataset_dir / f"{dataset_name}.json")
 
-    shutil.copyfile(
-        dataset_dir / f"{dataset_name}.json",
-        dataset_output_dir / f"{dataset_name}.json",
-    )
-    _tar_encrypted_dataset(input_dir=output_dir, dataset_name=dataset_name)
+        # Validate that there is only one csv file
+        if len(csv_files) > 1:
+            raise ValidationException(
+                f"There should only be one csv file in {dataset_dir}"
+            )
+
+        if len(csv_files) == 1:
+            # check if symkey exists
+            check_exists(dataset_dir / f"{dataset_name}.symkey.encr")
+
+            _encrypt_dataset(
+                rsa_keys_dir=rsa_keys_dir,
+                dataset_dir=dataset_dir,
+                output_dir=output_dir,
+                chunk_size_bytes=chunk_size_bytes,
+            )
+        else:
+            if not dataset_output_dir.exists():
+                os.makedirs(dataset_output_dir)
+
+        shutil.copyfile(
+            dataset_dir / f"{dataset_name}.json",
+            dataset_output_dir / f"{dataset_name}.json",
+        )
+        _tar_encrypted_dataset(input_dir=output_dir, dataset_name=dataset_name)
+
+    except Exception as exe:
+        logger.error(f"Failed to package dataset {dataset_name}: {exe}")
+        raise exe
