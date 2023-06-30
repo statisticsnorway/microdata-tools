950ab01712ed24e9c7c073fa9411ae59c2bebcc5
DanielElisenberg <Daniel@Elisenberg.com>
2023-06-30 13:32:30 +0200

    new_validator: added steps

---
+++ microdata_tools/validation/steps/data_reader.py
@@ -0,0 +1,155 @@
+# pylint: disable=no-member
+import logging
+from pathlib import Path
+from typing import Dict
+from datetime import datetime
+from datetime import timedelta
+
+import pyarrow
+from pyarrow import csv
+from pyarrow import compute, ArrowInvalid
+
+
+logger = logging.getLogger()
+
+
+def get_temporal_data(
+    table: pyarrow.Table, temporality_type: str
+) -> Dict[str, int]:
+    if temporality_type == "STATUS":
+        return {
+            "statusDates": compute.unique(table["epoch_start"]).to_pylist()
+        }
+    elif temporality_type == "FIXED":
+        stop_max = compute.max(table["epoch_stop"]).as_py().values()
+        return {
+            "start": None,
+            "latest": (
+                datetime(1970, 1, 1) + timedelta(days=stop_max)
+            ).strftime("%Y-%m-%d"),
+        }
+    else:
+        start_min, start_max = (
+            compute.min_max(table["epoch_start"]).as_py().values()
+        )
+        stop_min, stop_max = (
+            compute.min_max(table["epoch_stop"]).as_py().values()
+        )
+        min_date = min(
+            [date for date in [start_min, stop_min] if date is not None]
+        )
+        max_date = max(
+            [date for date in [start_max, stop_max] if date is not None]
+        )
+        return {
+            "start": (
+                datetime(1970, 1, 1) + timedelta(days=min_date)
+            ).strftime("%Y-%m-%d"),
+            "latest": (
+                datetime(1970, 1, 1) + timedelta(days=max_date)
+            ).strftime("%Y-%m-%d"),
+        }
+
+
+def get_csv_read_options():
+    return csv.ReadOptions(
+        column_names=["identifier", "measure", "start", "stop", "attributes"]
+    )
+
+
+def get_csv_convert_options(measure_data_type: str):
+    pyarrow_data_type = None
+    if measure_data_type == "STRING":
+        pyarrow_data_type = pyarrow.string()
+    elif measure_data_type == "LONG":
+        pyarrow_data_type = pyarrow.int64()
+    elif measure_data_type == "DOUBLE":
+        pyarrow_data_type = pyarrow.float64()
+    elif measure_data_type == "DATE":
+        pyarrow_data_type = pyarrow.date32()
+    else:
+        raise ValueError("TODO")
+    return csv.ConvertOptions(
+        column_types={
+            "identifier": pyarrow.string(),
+            "measure": pyarrow_data_type,
+            "start": pyarrow.date32(),
+            "stop": pyarrow.date32(),
+            "attributes": pyarrow.string(),
+        }
+    )
+
+
+def sanitize_data(
+    input_data_path: Path, measure_data_type: str
+) -> pyarrow.Table:
+    try:
+        table = csv.read_csv(
+            input_data_path,
+            parse_options=csv.ParseOptions(delimiter=";"),
+            read_options=get_csv_read_options(),
+            convert_options=get_csv_convert_options(measure_data_type),
+        )
+    except ArrowInvalid as e:
+        raise ValueError(str(e)) from e
+
+    identifier = compute.utf8_trim(table["identifier"], " ")
+    measure = (
+        table["measure"]
+        if measure_data_type != "STRING"
+        else compute.utf8_trim(table["measure"], " ")
+    )
+    epoch_start = compute.cast(table["start"], target_type=pyarrow.int32())
+    epoch_stop = compute.cast(table["stop"], target_type=pyarrow.int32())
+    start_year = compute.utf8_slice_codeunits(
+        table["start"].cast(pyarrow.string()), start=0, stop=4
+    )
+
+    # generate enriched table
+    return pyarrow.Table.from_arrays(
+        [
+            identifier,
+            measure,
+            start_year,
+            epoch_start,
+            epoch_stop,
+        ],
+        names=[
+            "identifier",
+            "measure",
+            "start_year",
+            "epoch_start",
+            "epoch_stop",
+        ],
+    )
+
+
+def _metadata_update_temporal_coverage(
+    metadata: dict, temporal_data: dict
+) -> None:
+    logger.debug(
+        "Append temporal coverage (start, stop, status dates) to metadata"
+    )
+    data_revision = metadata["dataRevision"]
+    temporality_type = metadata["temporalityType"]
+    data_revision["temporalCoverageStart"] = temporal_data["start"]
+    data_revision["temporalCoverageLatest"] = temporal_data["latest"]
+    if temporality_type == "FIXED":
+        data_revision["temporalCoverageStart"] = "1900-01-01"
+    if temporality_type == "STATUS":
+        temporal_status_dates_list = temporal_data["statusDates"]
+        temporal_status_dates_list.sort()
+        data_revision["temporalStatusDates"] = temporal_status_dates_list
+
+
+def run_reader(
+    dataset_name: str, input_directory: Path, measure_data_type: str
+) -> pyarrow.Table:
+    input_dataset_dir = input_directory / dataset_name
+    input_data_path = input_dataset_dir / f"{dataset_name}.csv"
+
+    logger.debug(f'Start reading dataset "{dataset_name}"')
+    table = sanitize_data(input_data_path, measure_data_type)
+
+    logger.debug(f'OK - reading dataset "{dataset_name}"')
+    return table
---
+++ microdata_tools/validation/steps/dataset_validator.py
@@ -0,0 +1,256 @@
+from typing import List, Union
+from pyarrow import dataset, compute, Table
+
+
+def valid_value_column_check(
+    parquet_path: str,
+    data_type: str,
+    code_list: Union[list, None],
+    sentinel_list: Union[List, None],
+):
+    """
+    Any given cell in the value column is valid only if:
+    * The cell contains a a valid non-null value
+    * The cell does not contain an empty string if data_type is STRING
+    * The value is present in the code_list if supplied
+    """
+    is_null_filter = dataset.field("measure").is_null()
+    is_empty_string_filter = dataset.field("measure") == ""
+    invalid_rows_filter = (
+        (is_null_filter | is_empty_string_filter)
+        if data_type == "STRING"
+        else is_null_filter
+    )
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=invalid_rows_filter, columns=["identifier"]
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError("valid_value_column_check")
+
+    if code_list:
+        unique_codes = list(
+            set(code_list_item["code"] for code_list_item in code_list)
+        ) + list(
+            set(
+                sentinel_list_item["code"]
+                for sentinel_list_item in sentinel_list
+            )
+        )
+        invalid_code_filter = ~dataset.field("measure").isin(unique_codes)
+        invalid_rows = dataset.dataset(parquet_path).to_table(
+            filter=invalid_code_filter, columns=["measure"]
+        )
+        if len(invalid_rows) > 0:
+            raise ValueError("invalid code list rows")
+
+
+def valid_unit_id_check(parquet_path: str):
+    """
+    Any given cell in the unit_id column is valid only if:
+    * The cell contains a a valid non-null value
+    * The cell does not contain an empty string
+    """
+    is_null_filter = dataset.field("identifier").is_null()
+    is_empty_string_filter = dataset.field("identifier") == ""
+    invalid_rows_filter = is_null_filter | is_empty_string_filter
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=invalid_rows_filter, columns=["identifier"]
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError("valid_unit_id_column_check")
+
+
+def fixed_temporal_variables_check(parquet_path: str):
+    """
+    Any given row in a table with temporalityType=FIXED is valid only if:
+    * The epoch_start column contains null (empty)
+    * The epoch_stop column contains a non-null value (int32)
+    """
+    start_is_valid_filter = dataset.field("epoch_start").is_valid()
+    stop_is_null_filter = dataset.field("epoch_stop").is_null()
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=start_is_valid_filter | stop_is_null_filter,
+        columns=["identifier"],
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError("valid_unit_id_column_check")
+
+
+def status_temporal_variables_check(parquet_path: str):
+    """
+    Any given row in a table with temporalityType=STATUS is valid only if:
+    * The epoch_start column contains a non-null value (int32)
+    * The epoch_stop column contains a non-null value (int32)
+    * The epoch_start and epoch_stop columns contain the same value
+      for any given row
+    """
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=dataset.field("epoch_start") != dataset.field("epoch_stop"),
+        columns=["identifier"],
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError("start did not equal stop")
+
+
+def event_temporal_variables_check(parquet_path: str):
+    """
+    Any given row in a table with temporalityType=EVENT is valid only if:
+    * The epoch_start column contains a non-null value (int32)
+    * The epoch_stop is either a non-null value bigger than
+      epoch_start (int32), or null (empty)
+    """
+    start_is_null_filter = dataset.field("epoch_start").is_null()
+    start_be_stop_filter = dataset.field("epoch_start") >= dataset.field(
+        "epoch_stop"
+    )
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=(start_is_null_filter | start_be_stop_filter),
+        columns=["identifier"],
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError(f"valid_event_temporal_columns_check {invalid_rows}")
+
+
+def accumulated_temporal_variables_check(parquet_path: str):
+    """
+    Any given row in a table with temporalityType=EVENT is valid only if:
+    * The epoch_start column contains a non-null value (int32)
+    * The epoch_stop is non-null (int32) value bigger than epoch_start
+    """
+    start_is_null_filter = dataset.field("epoch_start").is_null()
+    stop_is_null_filter = dataset.field("epoch_stop").is_null()
+    start_be_stop_filter = dataset.field("epoch_start") >= dataset.field(
+        "epoch_stop"
+    )
+    invalid_rows = dataset.dataset(parquet_path).to_table(
+        filter=(
+            start_is_null_filter | stop_is_null_filter | start_be_stop_filter
+        ),
+        columns=["identifier"],
+    )
+    if len(invalid_rows) > 0:
+        raise ValueError(
+            f"valid_accumulated_temporal_columns_check {invalid_rows}"
+        )
+
+
+def only_unique_identifiers_check(parquet_path: str):
+    """
+    A table with temporalityType=FIXED is only valid if all
+    cells in the unit_id column are unique.
+    """
+    identifiers = dataset.dataset(parquet_path).to_table(
+        columns=["identifier"],
+    )
+    identifiers = Table.from_arrays(
+        [
+            compute.utf8_slice_codeunits(
+                identifiers["identifier"], start=0, stop=1
+            ),
+            identifiers["identifier"],
+        ],
+        names=["bucket", "identifier"],
+    )
+    unique_buckets = compute.unique(identifiers["bucket"])
+    for unique_bucket in unique_buckets:
+        bucket_table = identifiers.filter(
+            dataset.field("bucket") == unique_bucket
+        )
+        bucket_row_count = len(bucket_table)
+        unique_identifiers_count = len(
+            compute.unique(bucket_table["identifier"])
+        )
+        if unique_identifiers_count != bucket_row_count:
+            raise ValueError("identifier diff")
+
+
+def status_uniquesness_check(parquet_path: str):
+    """
+    A table with temporalityType=STATUS is valid only if all
+    cells in the unit_id column are unique per status date.
+    """
+    all_status_dates = dataset.dataset(parquet_path).to_table(
+        columns=["epoch_start"],
+    )
+    unique_status_dates = compute.unique(all_status_dates["epoch_start"])
+    for status_date in unique_status_dates:
+        status_table = dataset.dataset(parquet_path).to_table(
+            columns=["identifier"],
+            filter=dataset.field("epoch_start") == status_date,
+        )
+        unique_identifiers = compute.unique(status_table["identifier"])
+        if len(unique_identifiers) != len(status_table):
+            raise ValueError("status uniqueness fail")
+
+
+def no_overlapping_timespans_check(parquet_path: str):
+    """
+    A table with temporalityType=(EVENT|ACCUMULATED) is valid
+    only if all rows for a given identifier contains no overlapping
+    timespans in the epoch_start and epoch_stop columns.
+    """
+
+    def find_overlap(start_list, stop_list):
+        """
+        Looks for overlapping timespans where each timespan
+        is defined by a start_date at an index from the start_list,
+        and a stop_date at the same index from the stop_list.
+        """
+        for i in range(len(start_list) - 1):
+            if stop_list[i] is None:
+                return True
+            if stop_list[i] > start_list[i + 1]:
+                return True
+        return False
+
+    def batch(iterable, batch_size):
+        for index in range(0, len(iterable), batch_size):
+            yield iterable[index : index + batch_size]
+
+    identifiers = dataset.dataset(parquet_path).to_table(
+        columns=["identifier"],
+    )
+    unique_identifiers = compute.unique(identifiers["identifier"])
+    for identifier_batch in batch(unique_identifiers, 5_000_000):
+        identifier_time_spans = dataset.dataset(parquet_path).to_table(
+            filter=dataset.field("identifier").isin(identifier_batch),
+            columns=["identifier", "epoch_start", "epoch_stop"],
+        )
+        identifier_time_spans = identifier_time_spans.sort_by(
+            [("epoch_start", "ascending")]
+        )
+        identifier_time_spans = identifier_time_spans.group_by(
+            "identifier"
+        ).aggregate([("epoch_start", "list"), ("epoch_stop", "list")])
+        for i in range(len(identifier_time_spans)):
+            if find_overlap(
+                identifier_time_spans["epoch_start_list"][i].as_py(),
+                identifier_time_spans["epoch_stop_list"][i].as_py(),
+            ):
+                raise ValueError("TODO")
+
+
+def validate_dataset(
+    parquet_path: str,
+    measure_data_type: str,
+    code_list: Union[List, None],
+    sentinel_list: Union[List, None],
+    temporality_type: str,
+) -> None:
+    valid_unit_id_check(parquet_path)
+    valid_value_column_check(
+        parquet_path, measure_data_type, code_list, sentinel_list
+    )
+    match temporality_type:
+        case "FIXED":
+            fixed_temporal_variables_check(parquet_path)
+            only_unique_identifiers_check(parquet_path)
+        case "STATUS":
+            status_temporal_variables_check(parquet_path)
+            status_uniquesness_check(parquet_path)
+        case "ACCUMULATED":
+            accumulated_temporal_variables_check(parquet_path)
+            no_overlapping_timespans_check(parquet_path)
+        case "EVENT":
+            event_temporal_variables_check(parquet_path)
+            no_overlapping_timespans_check(parquet_path)
