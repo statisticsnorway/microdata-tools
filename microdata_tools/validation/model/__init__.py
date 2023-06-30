e2dec74be9630d8730827ecc6c9c1a7d905ddbbf
DanielElisenberg <Daniel@Elisenberg.com>
2023-06-30 13:32:19 +0200

    new_validator: added models

---
+++ microdata_tools/validation/model/__init__.py
@@ -0,0 +1,30 @@
+# pylint: disable=raise-missing-from
+import logging
+from typing import Dict
+
+from pydantic import ValidationError
+
+from microdata_tools.validation.exceptions import InvalidDatasetException
+from microdata_tools.validation.model.metadata import Metadata
+
+
+logger = logging.getLogger()
+
+
+def _format_pydantic_error(error: dict) -> str:
+    location = "->".join(
+        loc for loc in error["loc"] if loc != "__root__" and not isinstance(loc, int)
+    )
+    return f'{location}: {error["msg"]}'
+
+
+def validate_metadata_model(metadata_json: Dict) -> Metadata:
+    try:
+        return Metadata(**metadata_json)
+    except ValidationError as e:
+        logger.exception(e)
+        error_messages = [_format_pydantic_error(error) for error in e.errors()]
+        raise InvalidDatasetException("Invalid metadata file", errors=error_messages)
+    except Exception as e:
+        logger.exception(e)
+        raise e
