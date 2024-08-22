ce471d212d17b6e892a1f264577d93ccbf1edd27
pawbu <pawbu@users.noreply.github.com>
2024-08-22 14:59:07 +0200

    temporal_end: be compatible with Python 3.8

--- microdata_tools/validation/model/metadata.py
+++ microdata_tools/validation/model/metadata.py
@@ -76,7 +76,7 @@ class MultiLingualString(BaseModel):
 
 class TemporalEnd(BaseModel):
     description: conlist(MultiLingualString, min_length=1)
-    successors: Optional[list[str]] = None
+    successors: Optional[List[str]] = None
 
 
 class DataRevision(BaseModel, extra="forbid"):
