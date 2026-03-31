1ca28619f1ad3b270c59fcdf295bd6179ce57c8e
Ivar Refsdal <ivar.refsdal@sikt.no>
2026-03-31 10:03:26 +0200

    WIP

---
+++ microdata_tools/validation/steps/dataset_accumulated.py
@@ -0,0 +1,331 @@
+# pyright: reportAttributeAccessIssue=false
+import logging
+import os.path
+import sqlite3
+import time
+from pathlib import Path
+from typing import Dict
+
+import psutil
+import pyarrow
+from pyarrow import compute, csv, dataset, parquet
+
+import microdata_tools.validation.steps.reader_utils as ru
+from microdata_tools.validation.exceptions import ValidationError
+
+logger = logging.getLogger()
+
+
+def current_milli_time() -> int:
+    return time.time_ns() // 1_000_000
+
+
+def log_time() -> int:
+    return time.time_ns() // 1_000_000 // 1000
+
+
+def get_temporal_data2(
+    table: pyarrow.Table, existing_data: Dict[str, int]
+) -> Dict[str, int]:
+    """
+    Reads the temporal columns of the pyarrow.Table and
+    returns a dictionary with information depending on the
+    temporality_type of the data.
+    """
+    start_min, start_max = (
+        compute.min_max(table["start_epoch_days"]).as_py().values()
+    )
+    stop_min, stop_max = (
+        compute.min_max(table["stop_epoch_days"]).as_py().values()
+    )
+    if start_min is None or start_max is None:
+        error_string = (
+            "Could not read data in third column (Start date)."
+            " Is this column empty?"
+        )
+        raise ValidationError(error_string, errors=[error_string])
+    if stop_min is None or stop_max is None:
+        error_string = (
+            "Could not read data in fourth column (Stop date)."
+            " Is this column empty?"
+        )
+        raise ValidationError(error_string, errors=[error_string])
+    min_date = min([date for date in [start_min, stop_min] if date is not None])
+    max_date = max([date for date in [start_max, stop_max] if date is not None])
+
+    temporal_data = {}
+    temporal_data["start"] = min(min_date, existing_data.get("start", min_date))
+    temporal_data["latest"] = max(
+        max_date, existing_data.get("latest", max_date)
+    )
+    return temporal_data
+
+
+def ms_to_eta(milliseconds: int) -> str:
+    seconds = milliseconds / 1000
+    (days, seconds) = divmod(seconds, int(24 * 3600))
+    (hours, seconds) = divmod(seconds, 3600)
+    (minutes, seconds) = divmod(seconds, 60)
+    if days > 0:
+        return (
+            f"{int(days):} days, {hours:02.0f}:{minutes:02.0f}:{seconds:02.0f}"
+        )
+    elif hours > 0:
+        return f"{hours:02.0f}:{minutes:02.0f}:{seconds:02.0f}"
+    else:
+        return f"{minutes:02.0f}:{seconds:02.0f}"
+
+
+def validate_unit_start_stop(unit_array: list) -> bool:
+    if len(unit_array) == 0:
+        return True
+    elif len(unit_array) == 1:
+        return True
+    else:
+        _, xs, xe = unit_array[0]
+        for _, ys, ye in unit_array[1:]:
+            if ys <= xs <= ye:  # xs inside ys,ye
+                return False
+            elif ys <= xe <= ye:  # xe inside ys,ye
+                return False
+        return validate_unit_start_stop(unit_array[1:])
+
+
+def validate_start_stop(row_count: int, conn: sqlite3.Connection) -> bool:
+    logger.info("Creating index ...")
+    start_ms = current_milli_time()
+    conn.execute("CREATE INDEX IF NOT EXISTS index_unit_id ON dataset(unit_id)")
+    conn.commit()
+    spent_ms = current_milli_time() - start_ms
+    logger.info(
+        f"Creating index ... Done in {spent_ms:_} ms aka {ms_to_eta(spent_ms)}"
+    )
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            "SELECT unit_id, start_day, stop_day FROM dataset ORDER BY unit_id"
+        )
+        processed_rows = 0
+        curr_unit = []
+        while True:
+            res = cursor.fetchone()
+            if res is None:
+                if len(curr_unit) != 0:
+                    validate_unit_start_stop(curr_unit)
+                break
+            elif len(curr_unit) == 0:
+                # first unit_id
+                curr_unit.append(res)
+            elif res[0] == curr_unit[0][0]:
+                # same unit_id
+                curr_unit.append(res)
+            else:
+                # different unit_id.
+                # first validate:
+                if not validate_unit_start_stop(curr_unit):
+                    logger.info("Bad bad bad")
+                # begin with new unit id:
+                curr_unit = [res]
+            processed_rows += 1
+            if processed_rows % 1_000_000 == 0:
+                logger.info(f"seen {processed_rows:_}")
+        return True
+    finally:
+        cursor.close()
+
+
+def _get_error_list(invalid_rows: pyarrow.Table, message: str) -> list[str]:
+    invalid_identifiers = (
+        invalid_rows.column("unit_id").slice(0, 50).to_pylist()
+    )
+    return [
+        f"{message} for row with identifier: {identifier}"
+        for identifier in invalid_identifiers
+    ]
+
+
+def csv_to_parquet(
+    identifier_data_type: str,
+    measure_data_type: str,
+    file_size: int,
+    row_count: int,
+    reader: pyarrow.csv.CSVStreamingReader,
+    writer: parquet.ParquetWriter,
+    conn: sqlite3.Connection,
+) -> Dict[str, int]:
+    start_time = current_milli_time()
+    temporal_data = {}
+    processed_rows = 0
+    last_log = -1
+    max_row_count_str = f"{row_count:_}"
+    process = psutil.Process(os.getpid())
+    logger.info("Beginning writing parquet file")
+    while True:
+        try:
+            batch = reader.read_next_batch()
+        except StopIteration:
+            break
+        if identifier_data_type == "STRING":
+            unit_id = compute.utf8_trim(batch["unit_id"], " ")
+        else:
+            unit_id = batch["unit_id"]
+        value = ru.sanitize_value2(batch, measure_data_type)
+        epoch_start = ru.cast_to_epoch_date2(batch, "start")
+        epoch_stop = ru.cast_to_epoch_date2(batch, "stop")
+        columns = [unit_id, value, epoch_start, epoch_stop]
+        column_names = [
+            "unit_id",
+            "value",
+            "start_epoch_days",
+            "stop_epoch_days",
+            "start_year",
+        ]
+        columns.append(ru.generate_start_year2(batch))
+        tbl = pyarrow.Table.from_arrays(columns, column_names)
+        temporal_data = get_temporal_data2(tbl, temporal_data)
+        processed_rows += len(tbl)
+        percent_done = (processed_rows * 100) / row_count
+        if last_log == log_time() and processed_rows != row_count:
+            pass
+        else:
+            last_log = log_time()
+            spent_ms_so_far = current_milli_time() - start_time
+            ms_per_row = spent_ms_so_far / processed_rows
+            remaining_ms = ms_per_row * (row_count - processed_rows)
+            mb_per_s = (
+                (file_size * (processed_rows / row_count)) / 1024 / 1024
+            ) / (spent_ms_so_far / 1000)
+            processed_rows_str = f"{processed_rows:_}".rjust(
+                len(max_row_count_str)
+            )
+            percent_done_str = f"{percent_done:.1f}".rjust(len("100.0"))
+            mb_per_s_str = f"{mb_per_s:.1f}".rjust(len("100.0"))
+            mem = process.memory_info()[0] // 1024 // 1024
+            mem_str = f"{mem}".rjust(len("1234"))
+            line = "".join(
+                [
+                    f"Processed {processed_rows_str} rows, ",
+                    f"{mem_str} MB mem, ",
+                    f"{mb_per_s_str} MB/s, ",
+                    f"{percent_done_str} % done. ",
+                    f"ETA: {ms_to_eta(remaining_ms)}",
+                ]
+            )
+            logger.info(line)
+        start_is_null_filter = dataset.field("start_epoch_days").is_null()
+        stop_is_null_filter = dataset.field("stop_epoch_days").is_null()
+        start_be_stop_filter = dataset.field(
+            "start_epoch_days"
+        ) >= dataset.field("stop_epoch_days")
+
+        invalid_rows = tbl.filter(
+            start_is_null_filter | stop_is_null_filter | start_be_stop_filter
+        )
+
+        if len(invalid_rows) > 0:
+            raise ValidationError(
+                "#3 and #4 columns",
+                errors=_get_error_list(
+                    invalid_rows, "Invalid #3 and/or #4 columns"
+                ),
+            )
+
+        sql_batch = []
+        for idx in range(len(tbl)):
+            unit_id_one = unit_id[idx]
+            start_day = epoch_start[idx].as_py()
+            stop_day = epoch_stop[idx].as_py()
+            sql_batch.append((str(unit_id_one), start_day, stop_day))
+        conn.executemany(
+            """INSERT INTO dataset (unit_id, start_day, stop_day)
+               VALUES (?, ?, ?)""",
+            sql_batch,
+        )
+        conn.commit()
+        batch_to_write = pyarrow.RecordBatch.from_arrays(columns, column_names)
+        writer.write_batch(batch_to_write)
+    return temporal_data
+
+
+def read_and_sanitize_csv2(
+    input_data_path: Path,
+    output_data_path: Path,
+    identifier_data_type: str,
+    measure_data_type: str,
+    temporality_type: str,
+) -> pyarrow.Table:
+    """
+    Reads a csv file to a pyarrow table. Sanitizes values and
+    ensures the input csv data follows the requirements for the
+    microdata data model.
+    """
+    start_time = current_milli_time()
+    logger.info(f"identifier_data_type: {identifier_data_type}")
+    logger.info(f"measure_data_type: {measure_data_type}")
+    logger.info(f"temporality_type: {temporality_type}")
+    row_count = ru.get_row_count(input_data_path)
+
+    logger.info(f"row_count: {row_count:_}")
+    file_size = input_data_path.stat().st_size
+    schema = pyarrow.schema(
+        [
+            (
+                "unit_id",
+                ru.microdata_data_type_to_pyarrow(identifier_data_type),
+            ),
+            ("value", ru.microdata_data_type_to_pyarrow(measure_data_type)),
+            ("start_epoch_days", pyarrow.int16()),
+            ("stop_epoch_days", pyarrow.int16()),
+            ("start_year", pyarrow.string()),
+        ]
+    )
+
+    if os.path.exists("janei.db"):
+        os.remove("janei.db")
+
+    with sqlite3.connect("janei.db", autocommit=False) as conn:
+        conn.execute(
+            """CREATE TABLE dataset(unit_id VARCHAR,
+                                    start_day INTEGER,
+                                    stop_day INTEGER)"""
+        )
+        conn.commit()
+        with parquet.ParquetWriter(output_data_path, schema) as writer:
+            with csv.open_csv(
+                input_data_path,
+                parse_options=csv.ParseOptions(delimiter=";"),
+                read_options=ru.get_csv_read_options(),
+                convert_options=ru.get_csv_convert_options(
+                    identifier_data_type, measure_data_type
+                ),
+            ) as reader:
+                csv_to_parquet(
+                    identifier_data_type,
+                    measure_data_type,
+                    file_size,
+                    row_count,
+                    reader,
+                    writer,
+                    conn,
+                )
+        logger.info("Done writing parquet file!")
+
+        validate_start_stop(row_count, conn)
+
+    # logger.debug(f"read_and_sanitize_csv row count: {len(table):_}")
+    # unit_id = _sanitize_unit_id(table, identifier_data_type)
+    # value = _sanitize_value(table, measure_data_type)
+    # epoch_start = _cast_to_epoch_date(table, "start")
+    # epoch_stop = _cast_to_epoch_date(table, "stop")
+    # columns = [unit_id, value, epoch_start, epoch_stop]
+    # column_names = ["unit_id", "value", "start_epoch_days", "stop_epoch_days"]
+    # if temporality_type in ["STATUS", "ACCUMULATED"]:
+    #     columns.append(_generate_start_year(table))
+    #     column_names.append("start_year")
+    # print(f'number of rows: {len(unit_id):_}')
+    # tbl = pyarrow.Table.from_arrays(columns, column_names)
+    spent_ms = current_milli_time() - start_time
+    mb_per_s = (file_size / 1024 / 1024) / (spent_ms / 1000)
+    logger.debug(f"read_and_sanitize_csv2 spent: {spent_ms:_} ms")
+    logger.debug(f"read_and_sanitize_csv2 speed: {mb_per_s:.1f} MB/s")
+    return None
