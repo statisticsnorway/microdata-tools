c0fdd005563776500f5a944cdb2fdc17c00cfc05
Daniel Elisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2026-09-14 16:19:33 +0200

    better_error_message: line breaks

--- microdata_tools/validation/steps/metadata_reader.py
+++ microdata_tools/validation/steps/metadata_reader.py
@@ -34,9 +34,10 @@ def _validate_datatype_in_codelist_and_sentinels(
     ]
     if invalid_codes:
         error_message = (
-            f"Specified data type for measure ({data_type}) does not match the "
-            f"data type within the {list_name} in the metadata json."
-            f"The codes appear to be of type {_determine_datatype(invalid_codes[0])}. "
+            f"Specified data type for measure ({data_type})"
+            f"does not match the data type within the {list_name}" 
+            "in the metadata json. The codes appear to be of type "
+            f"{_determine_datatype(invalid_codes[0])}. "
             "Codes with mismatching data type are: "
             f"{invalid_codes[:5] + ['...'] if len(invalid_codes) > 5 else invalid_codes}"  # noqa
         )
